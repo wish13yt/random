b6536c4f9d43b7a4c4820763ff72d94c54b39c9a
Wish <100239673+wish13yt@users.noreply.github.com>
2024-10-05 07:52:34 -0500

    modified:   index.html 	new file:   js/clear.js 	renamed:    confetti.js -> js/confetti.js 	new file:   js/gen.js

---
+++ js/clear.js
@@ -0,0 +1,7 @@
+function clearNum() {
+    let div = document.getElementById("div1");
+    div.replaceChildren();
+    console.log("cleared")
+    console.clear()
+    console.log("console cleared")
+}
---
+++ js/gen.js
@@ -0,0 +1,21 @@
+function randomGen() {
+    const minCeiled = Math.ceil(1);
+    const maxFloored = Math.floor(10);
+    num = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+    checkNumValue = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+    if (num === checkNumValue) {
+        console.log("number:" + num)
+        console.log("yuo won!!!!")
+        div1.innerHTML += num;
+        console.log("checknum:" + checkNumValue)
+        startConfetti();
+        alert("YOU WON!!!!")
+        document.getElementById("confettiButton").disabled = false;
+    } else {
+        console.log("yuo lost!111!!")
+        console.log("number:" + num)
+        div1.innerHTML += num;
+        console.log("checknum:" + checkNumValue)
+        alert("You lost!");
+    }
+}
