8b95aea0676b803b81f52e0ea0e44897f09dcaaf
Wish <100239673+wish13yt@users.noreply.github.com>
2024-10-05 19:10:18 -0500

    new file:   hard.html 	modified:   index.html 	new file:   js/genhard.js

---
+++ js/genhard.js
@@ -0,0 +1,27 @@
+function randomGen() {
+    const minCeiled = Math.ceil(1);
+    const maxFloored = Math.floor(100);
+    num = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+    checknum = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+    if (num === checknum) {
+        console.log("number:" + num)
+        console.log("yuo won!!!!")
+        div1.innerHTML += " " + num;
+        console.log("checknum:" + checknum)
+        startConfetti();
+        alert("YOU WON!!!!")
+        document.getElementById("confettiButton").disabled = false;
+        localStorage.setItem("num", num);
+localStorage.setItem("checknum", checknum);
+    } else {
+        console.log("yuo lost!111!!")
+        console.log("number:" + num)
+        div1.innerHTML += " " + num;
+        console.log("checknum:" + checknum)
+        alert("You lost!");
+        stopConfetti();
+        document.getElementById("confettiButton").disabled = true;
+        localStorage.setItem("num", num);
+localStorage.setItem("checknum", checknum);
+    }
+}
